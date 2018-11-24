70819b82058206a594a2ea5680df4dd94332f9af
Rafał Rudol <rrrrudol@gmail.com>
2018-11-25 00:18:09 +0100

    resolve conflicts

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const connect = require('./services/mongodb');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const connect = require("./services/mongodb");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3001;
-const path = require('path');
+const path = require("path");
 
-var cors = require('cors');
+var cors = require("cors");
 
 var whitelist = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'https://lootto.netlify.com',
-  'https://lootto.herokuapp.com'
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://lootto.netlify.com",
+  "https://lootto.herokuapp.com"
 ];
 
 var corsOptions = {
@@ -23,25 +23,32 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//database
+app.post("/register", (req, res) => {
+  res.send(
+    JSON.stringify({
+      ok: true
+    })
+  );
+});
+
 connect()
   .then(db => {
-    console.log('connected to mongodb');
+    console.log("connected to mongodb");
     app.listen(port, () =>
       console.log(`Example app listening on port ${port}!`)
     );
   })
   .catch(err => {
-    console.error('mongodb connection failed', err);
+    console.error("mongodb connection failed", err);
   });
 
 // routes
-const userController = require('./controller/UserController');
-const planetsController = require('./controller/PlanetController');
+const userController = require("./controller/UserController");
+const planetsController = require("./controller/PlanetController");
 
-app.use('/user', userController);
-app.use('/planet', planetsController);
+app.use("/user", userController);
+app.use("/planet", planetsController);
 
-app.get('*', function(request, response) {
-  response.sendFile(path.join(__dirname, 'build/index.html'));
+app.get("*", function(request, response) {
+  response.sendFile(path.join(__dirname, "build/index.html"));
 });
